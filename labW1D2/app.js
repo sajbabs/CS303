e6335abc5d3ef0551a43be8e2323f9f77be42a56
sajbabs <79480434+sajbabs@users.noreply.github.com>
2021-03-23 20:26:00 -0500

    day2 labs

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,19 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+    let count=0;
+    let arr=[];
+    for (let any in obj){
+        count+=1;
+        arr.push(obj[any]);
+    }
+
+    if (obj===undefined) return console.log("no Object passed");
+    else if (obj===null) return console.log("null arguement");
+    else if (count===0) return console.log("object has no properties");
+    else if (count>0)return console.log(count, arr.sort());
+
 }
